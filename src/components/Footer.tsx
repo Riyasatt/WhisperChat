9acf4fb7954efddfed808dc449758465d389be1a
Riyasatt <kriyasatali04@gmail.com>
2024-06-12 01:33:44 +0530

    Home Page and Footer Added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='w-full flex justify-center py-5'>
+    <div className='w-full bg-black  flex justify-center py-5'>
         made with ❤️ by &nbsp; <Link target='_blank' href='https://github.com/Riyasatt/WhisperChat' className='underline'>Riyasat</Link>
     </div>
   )
